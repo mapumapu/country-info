93179711f337d3d09ed11b8505cb2de6a42fc138
mapumapu <mapumapu@users.noreply.github.com>
2023-01-28 16:09:44 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+// AJAX Call: XMLHttpRequest
+
+let input = prompt("Enter country name: ");
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data);
+
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M People</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(
+              data.languages
+            )}</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>`;
+
+    // console.log(Object.values(data.currencies));
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData(input);
